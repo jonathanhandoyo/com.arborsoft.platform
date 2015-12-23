2c17d4344a5bf3ca113715266b8e2132454169df
Arnold Palar <arnold.palar@gmail.com>
2015-12-23 19:26:11 +0800

    optimize dependencies

--- web/src/main/java/com/arborsoft/platform/web/Application.java
+++ web/src/main/java/com/arborsoft/platform/web/Application.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
